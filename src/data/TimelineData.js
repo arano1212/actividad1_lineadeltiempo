c74e191d06f7dda7c42869952d3de1654e7eba36
arano1212 <arano1212@gmail.com>
2026-02-03 17:13:43 -0600

    primer commit

---
+++ src/data/TimelineData.js
@@ -0,0 +1,67 @@
+import img1950 from '../assets/images/1960.jpg'
+import img1960 from '../assets/images/19601.jpg'
+import img1970 from '../assets/images/1970.jpg'
+import img1980 from '../assets/images/1980.jpg'
+import img1990 from '../assets/images/1990.jpg'
+import img2000 from '../assets/images/2000.webp'
+import img2010 from '../assets/images/2010.jpg'
+import img2020 from '../assets/images/2020.jpg'
+
+const timelineData = [
+  {
+    year: "1940s – 1950s",
+    title: "Primeros usos de la informática",
+    description: `La administración informática se enfocaba en optimizar procesos básicos como cálculos científicos, contabilidad y nóminas. 
+    La finalidad era mejorar la eficiencia operativa dentro de las organizaciones.`,
+    image: img1950
+  },
+  {
+    year: "1960s",
+    title: "Eficiencia operativa",
+    description: `La informática se utilizaba para automatizar tareas repetitivas y reducir el uso de papel. 
+    La finalidad administrativa era apoyar la productividad interna.`,
+    image: img1960
+  },
+  {
+    year: "1970s",
+    title: "Organización y control",
+    description: `Surgen los primeros centros de cómputo y carreras universitarias en informática. 
+    La administración se enfocaba en organizar equipos y garantizar la continuidad operativa.`,
+    image: img1970
+  },
+  {
+    year: "1980s",
+    title: "Apoyo a la toma de decisiones",
+    description: `Se desarrollan sistemas de información gerencial (MIS) que proveen datos precisos para la administración. 
+    La informática comienza a tener un papel estratégico en la gestión empresarial.`,
+    image:img1980
+  },
+  {
+    year: "1990s",
+    title: "Integración con procesos de negocio",
+    description: `Con el auge de las redes y sistemas ERP/CRM, la finalidad administrativa fue alinear la informática con los procesos clave de las empresas.`,
+    image: img1990
+  },
+  {
+    year: "2000s",
+    title: "Ventaja competitiva",
+    description: `La informática se convierte en un diferenciador estratégico. 
+    Se busca innovar con TI, mejorar la experiencia del cliente y optimizar la cadena de valor.`,
+    image: img2000
+  },
+  {
+    year: "2010s",
+    title: "Gestión de riesgos y cumplimiento",
+    description: `La administración informática se centra en la ciberseguridad, protección de datos y cumplimiento de normativas internacionales como ISO/IEC 27001.`,
+    image: img2010
+  },
+  {
+    year: "2020s",
+    title: "Creación de valor e innovación",
+    description: `La informática se gestiona como recurso estratégico. 
+    Tecnologías emergentes como IA, IoT, Big Data y Cloud Computing impulsan la transformación digital y la sostenibilidad.`,
+    image: img2020
+  }
+];
+
+export default timelineData;
